227617e225076e812bc3711bde2c156986e24ab5
Lauritz Rasbach <35218654+LauritzR@users.noreply.github.com>
2022-12-27 20:09:01 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="principal-feature-analysis",
-    version="1.0.6",
+    name="parallel-principal-feature-analysis",
+    version="1.0.0",
     author="Tim Breitenbach & Lauritz Rasbach",
     author_email="tim.breitenbach@mathematik.uni-wuerzburg.de, rasbachlauritz@googlemail.com",
-    description="The first package for Principal Feature Analysis",
+    description="The first package for (parallel) Principal Feature Analysis",
     long_description=long_description,
     long_description_content_type="text/markdown",
     classifiers=[
