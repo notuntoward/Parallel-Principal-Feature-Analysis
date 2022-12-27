90a590c76f8cc9ef349c51f05c402b55ca783ecb
Lauritz Rasbach <35218654+LauritzR@users.noreply.github.com>
2022-12-27 19:59:02 +0100

    Update __init__.py

--- src/principal_feature_analysis/__init__.py
+++ src/principal_feature_analysis/__init__.py
@@ -1,4 +1,4 @@
-from .execute_PFA import pfa
+from .execute_PFA import par_pfa
 from .find_relevant_principal_features import find_relevant_principal_features
 from .get_mutual_information import get_mutual_information
 from .principal_feature_analysis import principal_feature_analysis
